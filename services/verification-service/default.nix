940ed85d0f17634082755a3b1a99e4e104d46673
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-01-24 09:30:47 +0100

    chore(verifiable-data-service): update nix and CI configuration

--- services/verification-service/default.nix
+++ services/verification-service/default.nix
@@ -1,10 +1,20 @@
+# Build with NIXPKGS_ALLOW_UNFREE=1 nix-build -E 'with import <nixpkgs> {}; callPackage ./default.nix {}'
 {
   pkgs ? import <nixpkgs> { },
 }:
 let
+  gitignoreSrc = pkgs.fetchFromGitHub {
+    # Documentation: https://github.com/hercules-ci/gitignore.nix
+    owner = "hercules-ci";
+    repo = "gitignore.nix";
+    rev = "637db329424fd7e46cf4185293b9cc8c88c95394";
+    # use what nix suggests in the mismatch message here:
+    sha256 = "sha256-HG2cCnktfHsKV0s4XW83gU3F57gaTljL9KNSuG6bnQs=";
+  };
+  inherit (import gitignoreSrc { inherit (pkgs) lib; }) gitignoreSource;
   manifest = (pkgs.lib.importTOML ./Cargo.toml).package;
 in
-pkgs.rustPlatform.buildRustPackage rec {
+pkgs.rustPlatform.buildRustPackage {
   pname = manifest.name;
   version = manifest.version;
   cargoLock.lockFile = ./Cargo.lock;
@@ -12,7 +22,10 @@ pkgs.rustPlatform.buildRustPackage rec {
   # "did-jwk-0.1.1" = pkgs.lib.fakeSha256;
   # "did-jwk-0.1.1" = "sha256-byxaWQDR35ioADSjWqGX/h8ht4FjXNh+mdtfD0LW8Sk=";
   # };
-  src = pkgs.lib.cleanSource ./.;
+  src = pkgs.lib.sources.cleanSourceWith {
+    src = gitignoreSource ./.;
+    filter = path: type: !(baseNameOf path == "target");
+  };
   nativeBuildInputs = with pkgs; [
     rustc
     rust-analyzer
