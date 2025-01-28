88ccc70e7bc15c98bd38d16a37c2d533dc213de5
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-01-28 09:51:20 +0100

    chore(verifiable-data-service): add tests #TG-148

--- services/verifiable-data-service/src/main.rs
+++ services/verifiable-data-service/src/main.rs
@@ -12,10 +12,14 @@ fn get_config() -> (String, u16) {
     (host, port)
 }
 
+pub fn create_app() -> Router {
+    Router::new().route("/", get(|| async { "Hello, World!" }))
+}
+
 #[tokio::main]
 async fn main() {
     // build our application with a single route
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = create_app();
 
     let (host, port) = get_config();
     let addr = format!("{}:{}", host, port)
@@ -23,7 +27,36 @@ async fn main() {
         .expect("Failed to parse address");
 
     // run our app with hyper, listening globally on port 3000
-    println!("Server starting on {}", addr);
+    println!("Listening on {}", addr);
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
     axum::serve(listener, app).await.unwrap();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use axum::{
+        body::Body,
+        http::{Request, StatusCode},
+    };
+    use http_body_util::BodyExt; // for `collect`
+    use tower::ServiceExt; // for `call`, `oneshot`, and `ready`
+
+    #[tokio::test]
+    async fn test_hello_world() {
+        // Create app for testing
+        let app = create_app();
+        // Create test request
+        let request = Request::builder().uri("/").body(Body::empty()).unwrap();
+
+        // Simulate request and get response
+        let response = app.oneshot(request).await.unwrap();
+
+        // Assert status code
+        assert_eq!(response.status(), StatusCode::OK);
+
+        // Convert bytes to string and assert content
+        let body = response.into_body().collect().await.unwrap().to_bytes();
+        assert_eq!(&body[..], b"Hello, World!");
+    }
 }
