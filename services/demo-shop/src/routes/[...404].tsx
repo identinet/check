1158392664ff3194bdb3ed976b7c6347cfd62ddf
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-03-31 09:28:28 +0200

    chore(demo-shop): return 404 error code when a page isn't found

--- services/demo-shop/src/routes/[...404].tsx
+++ services/demo-shop/src/routes/[...404].tsx
@@ -1,8 +1,10 @@
+import { HttpStatusCode } from "@solidjs/start";
 import { A } from "@solidjs/router";
 
 export default function NotFound() {
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
+      <HttpStatusCode code={404} />
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
         Not Found
       </h1>
