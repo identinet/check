6fe7f7e08ba1d5a03b0f03cb9e89eacc67fbc89d
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-04-04 11:57:31 +0200

    fix(demo-shop): remove setCounter from close button

--- services/demo-shop/src/routes/close.tsx
+++ services/demo-shop/src/routes/close.tsx
@@ -64,7 +64,6 @@ export default function Callback() {
           <button
             type="button"
             onclick={() => {
-              setCounter(counter() + 1);
               globalThis.close();
             }}
             class="flex w-full items-center justify-center rounded-lg bg-primary-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-primary-800 focus:outline-none focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 inactive"
