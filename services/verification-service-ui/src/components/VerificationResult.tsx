c63102025b52d473d2000540adf9ff55fe9c188c
Simon Mang <simne7@gmail.com>
2025-04-15 13:50:30 +0200

    feat(verification-service-ui): add static tabs above credential list #TG-226

--- services/verification-service-ui/src/components/VerificationResult.tsx
+++ services/verification-service-ui/src/components/VerificationResult.tsx
@@ -32,14 +32,53 @@ export default function VerificationResult({ pending, result, error }) {
   }
 
   return (
-    <div class="flex justify-center mt-4">
-      <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
-        <For each={credentials}>
-          {(credential) => (
-            <CredentialCard status={status} credential={credential} />
-          )}
-        </For>
+    <>
+      <div class="wfit mx-auto mt-8 text-sm font-medium text-center text-gray-900 border-b border-gray-200">
+        <ul class="flex flex-wrap -mb-px">
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block px-4 py-2 border-b-2 border-valid-900 rounded-t-lg active"
+              aria-current="page"
+            >
+              All credentials
+            </a>
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block px-4 py-2 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+            >
+              About the retailer
+            </a>
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block px-4 py-2 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+            >
+              Return & Disputes
+            </a>
+          </li>
+          <li class="me-2">
+            <a
+              href="#"
+              class="inline-block px-4 py-2 border-b-2 border-transparent rounded-t-lg hover:text-gray-600 hover:border-gray-300"
+            >
+              History
+            </a>
+          </li>
+        </ul>
       </div>
-    </div>
+      <div class="flex justify-center mt-8">
+        <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
+          <For each={credentials}>
+            {(credential) => (
+              <CredentialCard status={status} credential={credential} />
+            )}
+          </For>
+        </div>
+      </div>
+    </>
   );
 }
