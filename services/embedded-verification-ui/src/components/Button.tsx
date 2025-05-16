da3cc9954020c90645b3ac59d916b901f1f00cec
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-05-16 12:38:47 +0200

    fix(embedded-verification-ui): disable user-selection for button

--- services/embedded-verification-ui/src/components/Button.tsx
+++ services/embedded-verification-ui/src/components/Button.tsx
@@ -7,7 +7,7 @@ const Button: Component = (props) => {
       class="bg-white/80 border border-color-[#07348F] p-1 size-[2rem] rounded-full cursor-pointer"
     >
       <div
-        class="size-full bg-color-[#07348F] c-[#07348F] text-xl font-bold flex items-center justify-center"
+        class="select-none size-full bg-color-[#07348F] c-[#07348F] text-xl font-bold flex items-center justify-center"
         classList={{ [props.icon]: true }}
       >
         {props.children}
