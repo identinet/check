50cfd5a98e628cb9d3a0fc0f415e434c1575bcc0
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-05-16 14:26:51 +0200

    feat(demo-shop): embed evi

--- services/demo-shop/src/entry-server.tsx
+++ services/demo-shop/src/entry-server.tsx
@@ -9,10 +9,19 @@ export default createHandler(() => (
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href="/favicon.ico" />
+          {/* <link rel="stylesheet" href="https://evi.identinet.io.localhost/evi.css" /> */}
+          <link
+            rel="stylesheet"
+            href="https://evi.check.identinet.io/evi.css"
+          />
           {assets}
         </head>
         <body>
           <div id="app">{children}</div>
+          <div id="evi-identinet"></div>
+          {/* <script src="https://evi.identinet.io.localhost/evi.js" type="module"> */}
+          <script src="https://evi.check.identinet.io/evi.js" type="module">
+          </script>
           {scripts}
         </body>
       </html>
