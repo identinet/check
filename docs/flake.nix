884a9394e9a87f3ca51c0d1a8a011caef47de456
Jan Christoph Ebersbach <jan-christoph.ebersbach@identinet.io>
2025-06-13 14:46:42 +0200

    docs: add initial documentation page

---
+++ docs/flake.nix
@@ -0,0 +1,57 @@
+# Documentation: https://nixos.wiki/wiki/Flakes
+# Documentation: https://yuanwang.ca/posts/getting-started-with-flakes.html
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (self: super: {
+              unstable = import nixpkgs-unstable {
+                inherit system;
+                # config.allowUnfree = true;
+              };
+            })
+          ];
+        };
+      in
+      rec {
+        # Development environment: nix develop
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            # required for installing the sharp dependency
+            # https://sharp.pixelplumbing.com/
+            gcc
+            glib
+            pkg-config
+            vips
+          ];
+          nativeBuildInputs = with pkgs; [
+            deno
+            nodejs_22
+            just
+            nushell
+          ];
+        };
+        env = {
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+            pkgs.vips
+            pkgs.glib
+          ];
+        };
+      }
+    );
+}
