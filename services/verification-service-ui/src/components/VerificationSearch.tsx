e1793d94a889d4a4ec8648ea3c7b74598d56becc
Simon Mang <simne7@gmail.com>
2025-07-04 14:46:43 +0200

    feat(verification-service-ui): show error message from verification result

--- services/verification-service-ui/src/components/VerificationSearch.tsx
+++ services/verification-service-ui/src/components/VerificationSearch.tsx
@@ -96,6 +96,8 @@ const verifyUrlAction = action(async (formData: FormData) => {
   if (!response.ok) throw new Error(response.statusText);
 
   const result = await response.json();
+  if (result.error) throw new Error(result.error);
+
   return {
     ...result,
     results: [
